630ad42834260fd06f11b44cd91e56dcd044dcaf
subhanzakizada <zakizadasubhan@gmail.com>
2021-01-30 16:19:28 -0800

    implement infinite scroll logic

--- script.js
+++ script.js
@@ -1,49 +1,76 @@
+const loader = document.querySelector('.loader')
 const accessKey = 'DM7QQ7RJF8s-x7lfbfdF6_p-BikcD5s0CFQsUql_mvM'
-const count = 12 // count of photos requesting per time
+let count = 5 // count of photos requesting per time | count is 5 initially but after first time, it gets reassign to 30
 // Unsplash API URL
 const url = `https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${count}`
 
-let photos = [] // holds the photos' data which comes from getPhotos fn
+let allImages = [] // holds the photos' data which comes from getPhotos fn
+let loadedImages = 0 // gets increase when an image loads
+
+// variable shows that all images are loaded and if user scrolled down enough, displayImages fn will be called 
+let ready = false
 
 // Request Data
-async function getPhotos() {
+async function getImages() {
     const response = await fetch(url)
-    photos = await response.json()
-    console.log(photos)
+    allImages = await response.json() // array holds the data
+    count = 30
 }
 
 // Displaying photos on the UI
-async function displayPhotos() {
-    // Helper Function to set attributes
-    function setAttribute(el, attributes) { // ex:  setAttribute('img', { src: ..., alt: ... })
-        for (key in attributes) {
-            el.setAttribute(key, attributes[key])
+async function displayImages() {
+    try {
+        // On Load
+        await getImages()
+
+        // Helper Function to set attributes
+        function setAttribute(el, attributes) { // ex:  setAttribute('img', { src: ..., alt: ... })
+            for (key in attributes) {
+                el.setAttribute(key, attributes[key])
+            }
         }
+
+        const imgContainer = document.querySelector('.img-container')
+        allImages.forEach(image => {
+            // creating elements
+            const a = document.createElement('a') // anchor element
+            const imgEl = document.createElement('img')
+
+            // setting attributes
+            setAttribute(imgEl, {
+                src: image.urls.regular,
+                alt: image.alt_description,
+                title: image.alt_description
+            })
+            setAttribute(a, {
+                href: image.links.html,
+                target: '_blank',
+            })
+            
+            imgEl.addEventListener('load', function () {
+                loadedImages++
+                if (loadedImages === allImages.length) {
+                    ready = true
+                    loader.hidden = true
+                }
+            })
+
+            // anchor appends image then, image container appends anchor
+            a.appendChild(imgEl)
+            imgContainer.appendChild(a)
+        })
+    } catch (error) {
+        console.log(error)
+        alert('You\'ve done hourly max requests, please try again in an hour.')
     }
-
-    // On Load
-    await getPhotos()
-    const imgContainer = document.querySelector('.img-container')
-    photos.forEach(photo => {
-        // creating elements
-        const a = document.createElement('a') // anchor element
-        const img = document.createElement('img')
-
-        // setting attributes
-        setAttribute(img, {
-            src: photo.urls.regular,
-            alt: photo.alt_description,
-            title: photo.alt_description
-        })
-        setAttribute(a, {
-            href: photo.links.html,
-            target: '_blank',
-        })
-
-        // anchor appends image then, image container appends anchor
-        a.appendChild(img)
-        imgContainer.appendChild(a)
-    })
 }
 
-displayPhotos()
+displayImages()
+
+window.addEventListener('scroll', function () {
+    if (window.innerHeight + window.scrollY > document.body.offsetHeight / 2 && ready) {
+        displayImages()
+        loadedImages = 0
+        ready = false
+    }
+})
